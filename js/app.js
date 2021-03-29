3f450527695c8daf4642d2bb024331824c13dfca
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-03-29 21:29:53 +0300

    Add some style and logic

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 'use strict';
-let btn = document.getElementById('result_btn');
-
 const img_names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast',
-  'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen',
-  'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun',
-  'unicorn', 'usb', 'water-can', 'wine-glass'
+  'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon',
+  'pen', 'pet-sweep', 'scissors', 'shark', 'sweep',
+  'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'
 ];
 
-function randomNumber(min, max) {
+function randomIndex(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
@@ -16,88 +14,98 @@ const leftImage = document.getElementById('leftImage');
 const centerImage = document.getElementById('centerImage');
 const rightImage = document.getElementById('rightImage');
 
-const extension = ['jpg', 'png', 'gif'];
-function Data(name, extension) {
+// const extension = ['jpg', 'png', 'gif'];
+// function Prodcts(name, extension) {
+//   this.name = name;
+//   this.views = 0;
+//   this.extension = extension;
+//   this.path = `./img/${name}.${extension}`;
+//   this.votes = 0;
+//   Prodcts.holder.push(this);
+// }
+
+function Prodcts(name) {
   this.name = name;
+  if(name === 'usb'){
+    this.extension = '.gif';
+  } else if (name === 'sweep'){
+    this.extension = '.png';
+  }else{
+    this.extension = '.jpg';
+  }
+  this.path = './img/' + this.name + this.extension;
   this.views = 0;
-  this.extension = extension;
-  this.path = `./img/${name}.${extension}`;
   this.votes = 0;
-  Data.holder.push(this);
+  Prodcts.holder.push(this);
+}
+Prodcts.holder = [];
+
+for(let i = 0; i < img_names.length; i++){
+  new Prodcts(img_names[i]);
 }
 
-Data.holder = [];
+function randomImage() {
+  const leftIndex = randomIndex(0, Prodcts.holder.length - 1);
+  const leftRandomData = Prodcts.holder[leftIndex];
+  leftImage.src = leftRandomData.path;
+  leftImage.alt = leftRandomData.name;
+  leftImage.title = leftRandomData.name;
+  leftRandomData.views++;
 
-for (let z = 0; z < img_names.length; z++) {
-  if (img_names[z] === 'usb') {
-    new Data(img_names[z], extension[2]);
-  } else if (img_names[z] === 'sweep') {
-    new Data(img_names[z], extension[1]);
-  } else {
-    new Data(img_names[z], extension[0]);
+  const centerIndex = randomIndex(0, Prodcts.holder.length - 1);
+  if(centerIndex !== leftIndex){
+    const centerRandomData = Prodcts.holder[centerIndex];
+    centerImage.src = centerRandomData.path;
+    centerImage.alt = centerRandomData.name;
+    centerImage.title = centerRandomData.name;
+    centerRandomData.views++;
+  }
+
+  const rightIndex = randomIndex(0, Prodcts.holder.length - 1);
+  if(rightIndex !== centerIndex && rightIndex !== leftIndex){
+    const rightRandomData = Prodcts.holder[rightIndex];
+    rightImage.src = rightRandomData.path;
+    rightImage.alt = rightRandomData.name;
+    rightImage.title = rightRandomData.name;
+    rightRandomData.views++;
   }
 }
+randomImage();
 
-function render() {
-  const leftIndex = randomNumber(0, Data.holder.length - 1);
-  const leftRandomData = Data.holder[leftIndex];
-  leftImage.src = leftRandomData.path;
-  leftImage.title = leftRandomData.name;
-  leftImage.alt = leftRandomData.name;
-
-  const centerIndex = randomNumber(0, Data.holder.length - 1);
-  const centerRandomData = Data.holder[centerIndex];
-  centerImage.src = centerRandomData.path;
-  centerImage.title = centerRandomData.name;
-  centerImage.alt = centerRandomData.name;
-
-  const rightIndex = randomNumber(0, Data.holder.length - 1);
-  const rightRandomData = Data.holder[rightIndex];
-  rightImage.src = rightRandomData.path;
-  rightImage.title = rightRandomData.name;
-  rightImage.alt = rightRandomData.name;
-}
-
-render();
-
-section.addEventListener('click', progress);
-
-let x = 0;
-let ul = document.createElement('ul');
 let main = document.getElementById('main');
-let totalVotes;
+section.addEventListener('click', progress);
+let attempts = 25;
+let counter = 0;
 
 function progress(event) {
-  totalVotes = [];
   if (event.target.id === 'leftImage' || event.target.id === 'centerImage' || event.target.id === 'rightImage') {
-    for (let i = 0; i < Data.holder.length; i++) {
-      if (event.target.name[i] === event.target.title[i]) {
-        Data.holder[i].votes++;
+    for (let i = 0; i < Prodcts.holder.length; i++) {
+      if (Prodcts.holder[i].name === event.target.title) {
+        Prodcts.holder[i].votes++;
+        counter++;
       }
-      if (Data.holder[i].path === event.target.src || Data.holder[i].name === event.target.title) {
-        Data.holder[i].views++;
-      }
-      render();
     }
-    x++;
-
+    randomImage();
   } else {
     alert('clicked in image please!');
   }
 
-  if (x > 24) {
-    btn.style.visibility = 'visible';
+  if(counter === attempts) {
+    section.removeEventListener('click', progress);
+    let button = document.createElement('button');
+    button.style.visibility = 'visible';
+    main.appendChild(button);
+    button.textContent = 'Results';
+    button.addEventListener('click',resultDisplay);
+  }
+}
 
-    for (let i = 0; i < Data.holder.length; i++) {
-      section.removeEventListener('click', progress);
-      btn.addEventListener('click', function () {
-        btn.innerHTML = 'Hide Result';
-        let li = document.createElement('li');
-        li.innerHTML = (Data.holder[i].name + ' had ' + Data.holder[i].votes + ' votes, and was seen  ' + Data.holder[i].views + ' times.');
-        ul.appendChild(li);
-        main.appendChild(ul);
-      });
-    }
-    x = 0;
+function resultDisplay(){
+  let ul = document.createElement('ul');
+  main.appendChild(ul);
+  for (let i = 0; i < Prodcts.holder.length; i++) {
+    let li = document.createElement('li');
+    ul.appendChild(li);
+    li.innerText = Prodcts.holder[i].name + ' had ' + Prodcts.holder[i].votes + ' votes, and was seen  ' + Prodcts.holder[i].views + ' times.';
   }
 }
